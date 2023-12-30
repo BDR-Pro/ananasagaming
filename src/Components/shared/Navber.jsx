bfe3adb4671fd2935ba50f065d097e936afee6e0
reshad21 <miakala7@gmail.com>
2023-12-30 11:12:18 +0600

    navber create

--- src/Components/shared/Navber.jsx
+++ src/Components/shared/Navber.jsx
@@ -1,44 +1,29 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-
+import { IoSearch } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+import logo from '../../assets/react.svg';
 const Navber = () => {
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="flex justify-between items-center">
+            <img src={logo} alt="" width={150} height={150} />
+            <div className="w-[70%]">
+                <form action="w-full">
+                    <div className='relative'>
+                        <input type="text" placeholder="Search 860,872 games" className="input input-bordered w-full" />
+                        <button>
+                            <span className='absolute top-[50%] right-[1%] translate-x-[-0%] translate-y-[-50%]'>
+                                <IoSearch size={25} />
+                            </span>
+                        </button>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                </form>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+            <div className="">
+                <ul className='flex items-center gap-3'>
+                    <li><Link to="/login" className='btn btn-outline btn-sm'>Login</Link></li>
+                    <li><Link to="/signup" className='btn btn-outline btn-sm'>Signup</Link></li>
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
             </div>
         </div>
     );
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,3 +1,5 @@
+
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Components/shared/Footer';
