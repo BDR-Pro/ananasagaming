e8de97ce9c846f7f97fcb030c021543132aef150
reshad21 <miakala7@gmail.com>
2023-12-31 15:06:15 +0600

    json data added some new dfata

--- src/Components/Details/GameDetails.jsx
+++ src/Components/Details/GameDetails.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useDataFetcher from '../../hook/useDataFetcher';
 
 const GameDetails = () => {
     const { id } = useParams();
-    console.log(id);
+    const { data, error, loading } = useDataFetcher(`http://localhost:5000/trending/${id}`);
+    console.log(data);
     return (
         <div className='pt-20 px-10 min-h-screen bg-blue-950'>
             all game infromation comes here
